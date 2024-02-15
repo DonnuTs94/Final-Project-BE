6e7256fbb27446a951ef18a685e498c17b6ce5c3
Inun572 <helmi.ainun@gmail.com>
2024-02-15 17:38:53 +0700

    add createToken and verifyToken jwt

---
+++ app/helpers/token.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const createToken = (userData) => {
+  const { userId, roleId } = userData;
+
+  if (typeof userId !== 'number' || typeof roleId !== 'number') {
+    throw new Error('UserId and roleId must be a number');
+  }
+  return jwt.sign({ userId, roleId }, process.env.JWT_SECRET, {
+    expiresIn: `${process.env.JWT_EXPIRES_IN}`,
+  });
+};
+
+export const verifyToken = (token) => {
+  if (typeof token !== 'string') {
+    throw new Error('Token must be a string');
+  }
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
