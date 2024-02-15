19fa462f4dd52eaf1c9189ab49072418e1944bdc
DonnuTs69 <111718022+DonnuTs69@users.noreply.github.com>
2024-02-15 21:35:15 +0700

    add authorization and seeder

---
+++ app/constants/authorization.js
@@ -0,0 +1,27 @@
+export const Role = {
+  ADMIN: "admin",
+  USER: "user",
+}
+
+export const Permission = {
+  BROWSE_PRODUCTS: "browse_products",
+  READ_PRODUCT: "read_product",
+  EDIT_PRODUCT: "edit_product",
+  ADD_PRODUCT: "add_product",
+  DELETE_PRODUCT: "delete_product",
+
+  BROWSE_CATEGORIES: "browse_categories",
+  READ_CATEGORY: "read_category",
+  EDIT_CATEGORY: "edit_category",
+  ADD_CATEGORY: "add_category",
+  DELETE_CATEGORY: "delete_category",
+
+  BROWSE_CARTS: "browse_carts",
+  EDIT_CART: "edit_cart",
+  ADD_CART: "add_cart",
+  DELETE_CART: "delete_cart",
+
+  BROWSE_ORDERS: "browse_orders",
+  READ_ORDER: "read_order",
+  ADD_ORDER: "add_order",
+}
--- app/middlewares/loginMiddleware.js
+++ app/middlewares/loginMiddleware.js
@@ -1,69 +1,68 @@
-import { REGEX_EMAIL, REGEX_PASSWORD } from '../constants/regexValidator.js';
-import { findUser } from '../services/userServices.js';
-import bcrypt from 'bcrypt';
+import { REGEX_EMAIL, REGEX_PASSWORD } from "../constants/regexValidator.js"
+import { findUser } from "../services/userServices.js"
+import bcrypt from "bcrypt"
 
 export const validateLoginRequestBody = (req, res, next) => {
-  if (!req.body.email && !req.body.password) {
+  const { email, password } = req.body
+  if (!email && !password) {
     return res.status(400).json({
-      message: 'Please provide email and password',
-    });
+      message: "Please provide email and password",
+    })
   }
 
-  if (!req.body.email) {
+  if (!email) {
     return res.status(400).json({
-      message: 'Please provide an email',
-    });
+      message: "Please provide an email",
+    })
   }
 
-  if (!req.body.password) {
+  if (!password) {
     return res.status(400).json({
-      message: 'Please provide password',
-    });
+      message: "Please provide password",
+    })
   }
-
-  const { email, password } = req.body;
 
   if (email) {
-    const emailPattern = REGEX_EMAIL;
+    const emailPattern = REGEX_EMAIL
 
     if (emailPattern.test(email) === false) {
       return res.status(400).json({
-        message: 'Please provide a valid email format',
-      });
+        message: "Please provide a valid email format",
+      })
     }
   }
 
-  const passwordPattern = REGEX_PASSWORD;
+  const passwordPattern = REGEX_PASSWORD
   if (passwordPattern.test(password) === false) {
     return res.status(400).json({
       message:
-        'Password must contain at least 8 characters, alphanumeric, one uppercase letter, one lowercase letter and symbol',
-    });
+        "Password must contain at least 8 characters, alphanumeric, one uppercase letter, one lowercase letter and symbol",
+    })
   }
-  next();
-};
+  next()
+}
 
 export const validateLogin = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
-  const user = await findUser(email);
+  const user = await findUser(email)
 
   if (!user) {
     return res.status(401).json({
-      message: 'User not found',
-    });
+      message: "User not found",
+    })
   }
 
   if (!(await bcrypt.compare(password, user.password))) {
     return res.status(401).json({
-      message: 'Wrong password',
-    });
+      message: "Wrong password",
+    })
   }
 
   req.user = {
     userId: user.id,
     roleId: user.roleId,
-  };
+  }
 
-  next();
-};
+  next()
+}
