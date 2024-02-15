b32ae458877bec0e292e5acf7c5e9ef48cbd0a7d
Inun572 <helmi.ainun@gmail.com>
2024-02-15 20:12:45 +0700

    add validateToken to check authentication

---
+++ app/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+import pkg from 'jsonwebtoken';
+import { verifyToken } from '../helpers/token.js';
+
+const { TokenExpiredError } = pkg;
+
+export const validateToken = (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.status(401).json({
+        message: 'Unauthorized',
+      });
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    const userData = verifyToken(token);
+
+    req.user = userData;
+    next();
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      return res.status(401).json({
+        message: 'Token has expired',
+      });
+    }
+
+    next(err);
+  }
+};
