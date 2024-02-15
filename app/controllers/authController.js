6194ee3ba7a598a8d196bcac78836fa51ab11c35
Inun572 <helmi.ainun@gmail.com>
2024-02-15 17:40:25 +0700

    add login funtionality

---
+++ app/controllers/authController.js
@@ -0,0 +1,20 @@
+import { createToken } from '../helpers/token.js';
+
+export const login = async (req, res) => {
+  try {
+    if (req.user !== undefined) {
+      const { userId, roleId } = req.user;
+      const token = createToken({ userId, roleId });
+
+      res.status(200).json({
+        message: 'Login Success',
+        token,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Internal Server Error',
+      error: err.message,
+    });
+  }
+};
