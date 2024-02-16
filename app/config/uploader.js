74079ad05496c9368f547fc1fc6c254b050aa00b
DonnuTs69 <febrykurniawanhakim@gmail.com>
2024-02-16 13:38:11 +0700

    create multer and validate product body

--- app/config/uploader.js
+++ app/config/uploader.js
@@ -1,1 +1,36 @@
 import multer from "multer"
+
+export const upload = ({
+  filePrefix = "FILE",
+  fileName = Date.now(),
+  acceptedFileTypes = [],
+  maxSize,
+  dynamicDestination,
+}) => {
+  const diskStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      const dynamicPath = dynamicDestination
+      cb(null, dynamicPath)
+    },
+    filename: (req, file, cb) => {
+      const { originalname } = file
+      fileName = originalname + Date.now()
+      cb(null, `${filePrefix}=${fileName}.${file.mimetype.split("/")[1]}`)
+    },
+  })
+
+  const fileFilter = (req, file, cb) => {
+    const extension = file.mimetype.split("/")[1]
+
+    if (acceptedFileTypes.includes(extension)) {
+      cb(null, true)
+    } else {
+      cb(new Error("invalid file type"))
+    }
+  }
+  return multer({
+    storage: diskStorage,
+    fileFilter,
+    limits: { fileSize: maxSize },
+  })
+}
---
+++ app/middlewares/productMidleware.js
@@ -0,0 +1,13 @@
+const validateInputProduct = (req, res, next) => {
+  const { name, quantity, price, description, categoryId } = req.body
+
+  if (!name || !quantity || !price || !description || categoryId) {
+    return res.status(400).json({
+      message: "Input must be filled!",
+    })
+  }
+
+  next()
+}
+
+export { validateInputProduct }
