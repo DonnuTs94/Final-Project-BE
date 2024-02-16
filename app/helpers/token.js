9ff4789bbf787294762c5302e95b1fb654f66c96
Inun572 <helmi.ainun@gmail.com>
2024-02-16 11:44:47 +0700

    refactor create and verify token

--- app/helpers/token.js
+++ app/helpers/token.js
@@ -6,17 +6,11 @@ dotenv.config()
 export const createToken = (userData) => {
   const { userId, roleId } = userData
 
-  if (typeof userId !== "number" || typeof roleId !== "number") {
-    throw new Error("UserId and roleId must be a number")
-  }
   return jwt.sign({ userId, roleId }, process.env.JWT_SECRET, {
     expiresIn: `${process.env.JWT_EXPIRES_IN}`
   })
 }
 
 export const verifyToken = (token) => {
-  if (typeof token !== "string") {
-    throw new Error("Token must be a string")
-  }
   return jwt.verify(token, process.env.JWT_SECRET)
 }
