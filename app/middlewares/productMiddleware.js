c550021847f53559c2975d3c9ca0e3779656ea64
Inun572 <helmi.ainun@gmail.com>
2024-02-17 18:41:16 +0700

    add getProductById and softDeleteProducts

--- app/middlewares/productMiddleware.js
+++ app/middlewares/productMiddleware.js
@@ -37,4 +37,22 @@ const validateInputProduct = async (req, res, next) => {
   next()
 }
 
-export { validateInputProduct }
+const validateParamsProduct = async (req, res, next) => {
+  const { id } = req.params
+
+  if (!id) {
+    return res.status(400).json({
+      message: "Product id is required"
+    })
+  }
+
+  if (isNaN(Number(id))) {
+    return res.status(400).json({
+      message: "Product id must be a number"
+    })
+  }
+
+  next()
+}
+
+export { validateInputProduct, validateParamsProduct }
