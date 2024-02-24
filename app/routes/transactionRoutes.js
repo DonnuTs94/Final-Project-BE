414f6c016299c60c9670a7d2cd63a0e832c7b3a5
AndrianaRzky16 <andrianarizki42@gmail.com>
2024-02-24 15:35:26 +0700

    Payment Midtrans Webhook

--- app/routes/transactionRoutes.js
+++ app/routes/transactionRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from "express"
-import validateTransaction from "../middlewares/transactionMiddleware.js"
+// import validateTransaction from "../middlewares/transactionMiddleware.js"
 import PostCreateTransaction from "../controllers/paymentController.js"
 
 const router = Router()
 
-router.post(
-  "/transaction",
-  validateTransaction("createTransaction"),
-  PostCreateTransaction
-)
+router.post("/transaction", PostCreateTransaction.createPayment)
 
 export default router
